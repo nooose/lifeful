f61baf9ac3465bfd0fb42c561f7f7561b55c6eb1
dltkqnr <dltkqnr@naver.com>
2025-05-22 20:47:42 +0900

    feat(club): Club 생성 및 조회 API 수정
    - 읽기 회차(ReadingStep) 추가에 따른 로직 추가
    - 회차 별로 정해진 챕터로 읽기 회차 추가

--- src/main/kotlin/readful/core/application/club/command/ClubCommandService.kt
+++ src/main/kotlin/readful/core/application/club/command/ClubCommandService.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service
 import readful.core.domain.book.Book
 import readful.core.domain.club.Club
 import readful.core.domain.club.ClubRepository
+import readful.core.domain.club.reading.ReadingStep
 import java.time.LocalDateTime
 
 @Service
@@ -16,9 +17,13 @@ class ClubCommandService(
       description: String,
       memberCount: Int,
       startAt: LocalDateTime,
-      book: Book
+      book: Book,
+      steps: List<ReadingStep>
   ): Club {
       val club = Club.create(title, description, memberCount, startAt, book);
+
+      steps.forEach{ step -> club.addReadingStep(step) }
+
       return clubRepository.save(club)
   }
 }
