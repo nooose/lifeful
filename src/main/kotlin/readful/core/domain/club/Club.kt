d6f3aa9ed47b5c19f5933d26bdaa840ea7806795
dltkqnr <dltkqnr@naver.com>
2025-05-28 23:28:23 +0900

    feat(club): club 도메인 수정
    # 도메인 수정
    - 불필요한 startAt 필드 제거
    - readingStep, book 과의 연관관계 제거

--- src/main/kotlin/readful/core/domain/club/Club.kt
+++ src/main/kotlin/readful/core/domain/club/Club.kt
@@ -6,50 +6,27 @@ import readful.core.domain.book.BookChapter
 import readful.core.domain.club.reading.ReadingStep
 import readful.core.domain.shared.BaseEntity
 import readful.core.domain.shared.ClubId
-import java.time.LocalDateTime
 
 @Entity
 class Club(
-    val title: String,
-    val description: String,
-    val memberCount: Int,
-    val startAt: LocalDateTime,
-
+    var title: String,
+    var description: String,
+    var memberMaxCount: Int,
+    val hostId: Int,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id : ClubId = ClubId()
-): BaseEntity() {
+    val id: ClubId = ClubId(),
+    members: List<ClubMember> = emptyList(),
+) : BaseEntity() {
+
+    @OneToMany
+    val members: MutableList<ClubMember> = members.toMutableList()
+
     init {
-        require( memberCount > 0 ) {"정원은 1명 이상이여야 합니다."}
-        require(startAt.isAfter(LocalDateTime.now())) {"시작일은 현재보다 미래여야 합니다."}
+        require(memberMaxCount > 0) { "정원은 1명 이상이여야 합니다." }
     }
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "book_id", nullable = false)
-    lateinit var book : Book
-
-    @OneToMany(mappedBy = "club", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val readingSteps: MutableList<ReadingStep> = mutableListOf()
-
-    fun assignBook(book: Book){
-        this.book = book;
-    }
-
-    fun addReadingStep(step: ReadingStep){
-        step.assignClub(this)
-        readingSteps.add(step)
-    }
-
-    companion object {
-        fun create(
-            title: String,
-            description: String,
-            memberCount: Int,
-            startAt: LocalDateTime,
-            book: Book
-        ): Club {
-            return Club(title, description, memberCount, startAt).apply {
-                assignBook(book)
-            }
-        }
+    fun requestJoin(memberId: Int) {
+        val pendingMember = ClubMember.pending(clubId = this.id, memberId = memberId)
+        this.members.add(pendingMember)
     }
 }
