f7a7e87d511ba46b8311fdfdf27272d31310d1f2
nooose <hd15807@gmail.com>
2025-06-10 21:54:07 +0900

    feat(api): API 문서화와 구현체 분리

--- modules/api/src/main/kotlin/lifeful/api/book/BookRestController.kt
+++ modules/api/src/main/kotlin/lifeful/api/book/BookRestController.kt
@@ -1,10 +1,6 @@
 package lifeful.api.book
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
-import io.swagger.v3.oas.annotations.tags.Tag
 import lifeful.book.application.query.FindBook
-import lifeful.support.RequiredAuthorization
 import lifefule.shared.BookId
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,42 +8,24 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequiredAuthorization
-@Tag(
-    name = "도서 API",
-    description = "도서 관련 API",
-)
 @RequestMapping("/api/v1")
 @RestController
 internal class BookRestController(
     private val findBook: FindBook,
-) {
-    @Operation(
-        summary = "책 목록 조회",
-        operationId = "getBooks",
-    )
+) : BookApi {
     @GetMapping("/books")
-    fun getBooks(): List<BookResponse> {
+    override fun getBooks(): List<BookResponse> {
         val books = findBook.all()
         return books.map { BookResponse.Companion.from(it) }
     }
 
-    @Operation(
-        summary = "책 등록",
-        operationId = "addBook",
-    )
     @PostMapping("/books")
-    fun addBook() {
+    override fun addBook() {
         TODO("책 추가 구현")
     }
 
-    @Operation(
-        summary = "책 상세 조회",
-        operationId = "getBook",
-    )
     @GetMapping("/books/{bookId}")
-    fun getBook(
-        @Parameter(description = "책 식별자")
+    override fun getBook(
         @PathVariable bookId: BookId,
     ): BookResponse {
         TODO("책 상세 조회 구현")
--- modules/api/src/main/kotlin/lifeful/api/review/ReviewRestController.kt
+++ modules/api/src/main/kotlin/lifeful/api/review/ReviewRestController.kt
@@ -1,8 +1,5 @@
 package lifeful.api.review
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
-import io.swagger.v3.oas.annotations.tags.Tag
 import java.net.URI
 import lifeful.review.application.AddReview
 import lifeful.review.application.FindReview
@@ -20,24 +17,15 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(
-    name = "후기 API",
-    description = "후기 관련 API",
-)
 @RequestMapping("/api/v1")
 @RestController
 class ReviewRestController(
     private val addReview: AddReview,
     private val findReview: FindReview,
     private val modifyReview: ModifyReview,
-) {
-    @Operation(
-        summary = "후기 등록",
-        operationId = "review",
-    )
+) : ReviewApi {
     @PostMapping("/books/{bookId}/reviews")
-    fun review(
-        @Parameter(description = "책 식별자")
+    override fun review(
         @PathVariable bookId: BookId,
         @RequestBody request: ReviewAddRequest,
     ): ResponseEntity<Unit> {
@@ -53,43 +41,26 @@ class ReviewRestController(
         return ResponseEntity.created(location).build()
     }
 
-    @Operation(
-        summary = "후기 목록 조회",
-        operationId = "getReviews",
-    )
     @GetMapping("/books/{bookId}/reviews")
-    fun getReviews(
-        @Parameter(description = "책 식별자")
+    override fun getReviews(
         @PathVariable bookId: BookId,
     ): List<ReviewSummaryResponse> {
         val reviews = findReview.all(bookId = bookId)
         return reviews.map { ReviewSummaryResponse.from(it) }
     }
 
-    @Operation(
-        summary = "후기 상세 조회",
-        operationId = "getReview",
-    )
     @GetMapping("/books/{bookId}/reviews/{reviewId}")
-    fun getReview(
-        @Parameter(description = "책 식별자")
+    override fun getReview(
         @PathVariable bookId: BookId,
-        @Parameter(description = "후기 식별자")
         @PathVariable reviewId: ReviewId,
     ): ReviewResponse {
         val review = findReview.byId(bookId = bookId, reviewId = reviewId)
         return ReviewResponse.from(review)
     }
 
-    @Operation(
-        summary = "후기 수정",
-        operationId = "editReview",
-    )
     @PatchMapping("/books/{bookId}/reviews/{reviewId}")
-    fun editReview(
-        @Parameter(description = "책 식별자")
+    override fun editReview(
         @PathVariable bookId: BookId,
-        @Parameter(description = "후기 식별자")
         @PathVariable reviewId: ReviewId,
         @RequestBody request: ReviewEditRequest,
     ) {
